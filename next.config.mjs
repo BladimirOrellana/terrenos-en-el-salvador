e5d9f95adb637845b3c7d7fe7bf99303302a39c0
Bladimir Orellana <me@bladimirorellana.com>
2025-02-27 02:34:30 -0600

    Fixed next.config.mjs structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPWA from "next-pwa";
 
-export default nextConfig;
+export default {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true, // Ensures App Router is enabled
+  },
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+  },
+};
